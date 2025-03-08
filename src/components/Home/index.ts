29854210dc2960db529d0ff0b5af7e2341bd78f6
ManasMadan <manasmadan08@gmail.com>
2025-03-09 00:33:27 +0530

    draft 1

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -2,8 +2,7 @@ import AboutUs from "./About";
 import AfterMovie from "./AfterMovie";
 import FAQSection from "./FAQ";
 import Gallery from "./Gallery";
-import Hero from "./Hero";
 import PastSponsor from "./PastSponsor";
 import Footer from "./Footer";
 
-export { AboutUs, AfterMovie, FAQSection, Gallery, Hero, PastSponsor, Footer };
+export { AboutUs, AfterMovie, FAQSection, Gallery, PastSponsor, Footer };
